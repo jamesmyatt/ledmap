f80fa0c4f0a0dc81339f572e890d8369377b0eb8
James Myatt <james@jamesmyatt.co.uk>
2025-11-14 14:57:06 +0000

    Add wrappers for mappings and mappers

--- src/ledmap/pixels.py
+++ src/ledmap/pixels.py
@@ -1,7 +1,9 @@
 """Pixel mappings."""
 
 import itertools
-from collections.abc import Iterable
+from collections.abc import Callable, Iterable, Iterator
+from dataclasses import dataclass
+from typing import Any, Concatenate
 
 import numpy as np
 
@@ -49,3 +51,87 @@ def as_string(
 
     msg = "Shape not supported"
     raise ValueError(msg)
+
+
+@dataclass
+class Mapping:
+    """Pixel map from mapping array."""
+
+    array: np.ndarray
+
+    @classmethod
+    def from_shape(cls, shape: tuple[int, ...], *, vertical: bool = False):
+        """Create default mapping from shape."""
+        if not all(s > 0 for s in shape):
+            msg = "All dimensions must be strictly positive."
+            raise ValueError(msg)
+
+        length = np.prod(shape, dtype=int)
+        order = "F" if vertical else "C"
+        array = np.arange(length, int).reshape(shape, order=order)
+
+        return cls(array)
+
+    @property
+    def ndim(self) -> int:
+        """Number of array dimensions."""
+        return self.array.ndim
+
+    @property
+    def shape(self) -> tuple[int, ...]:
+        """Shape of array."""
+        return self.array.shape
+
+    def apply(
+        self,
+        func: Callable[Concatenate[np.ndarray, ...], np.ndarray],
+        *args: Any,
+        **kwargs: Any,
+    ) -> "Mapping":
+        """Apply function to mapping array."""
+        array = func(self.array, *args, **kwargs)
+        return Mapping(array)
+
+
+@dataclass
+class Mapper:
+    """Pixel map from mapping function."""
+
+    shape: tuple[int, ...]
+
+    def index(self) -> Iterator[tuple[int, ...]]:
+        """Iterate over location indices."""
+        ranges = (range(n) for n in self.shape)
+        return itertools.product(*ranges)
+
+    def iter(self) -> Iterator[int]:
+        """Iterate over pixel indices."""
+        for loc in self.index():
+            yield self.get(*loc)
+
+    def __iter__(self) -> Iterator[int]:
+        """Iterate over pixel indices."""
+        yield from self.iter()
+
+    def to_array(self) -> np.ndarray:
+        """Convert to NumPy array."""
+        return np.ndarray(list(self), int).reshape(self.shape)
+
+    def to_mapping(self) -> Mapping:
+        """Convert to mapping object."""
+        return Mapping(self.to_array())
+
+    def get(self, *loc: int) -> int:
+        """Get pixel index at location.
+
+        Override this method for different pixel mappings.
+
+        Default is row-major index.
+        """
+        out: int = 0
+        k: int = 1
+        for i, n in zip(loc, self.shape, strict=True):
+            assert 0 <= i < n
+            out += i * k
+            k *= n
+        return out
